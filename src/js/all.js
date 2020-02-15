9cf682a07d7c1ff42931db0c08f4f2330e0d9195
shunnNet <wendell20904102@gmail.com>
2020-02-16 00:01:34 +0800

    sort js code

--- src/js/all.js
+++ src/js/all.js
@@ -1,72 +1,86 @@
 $(document).ready(function () {
-    // menu slide bar effect --- horizontal
-    $(".navMenu > .navMenu__item").hover((event)=>{
-        let parent = event.currentTarget;
-        let center = parent.clientWidth / 2;
-        let enterPoint = event.offsetX;
-        let fromleft = enterPoint <= center;
-        menuSlide(parent,fromleft,true)
-    },(event)=>{
-        menuSlideBack(event.currentTarget,true)
-    })
-    // menu slide bar effect --- vertical
-    $(".navMenu__children > .navMenu__item").hover((event)=>{
-        let parent = event.currentTarget;
-        let center = parent.clientHeight / 2;
-        let enterPoint = event.offsetY;
-        let fromTop = enterPoint <= center;
-        menuSlide(parent,fromTop,false)
-    },(event)=>{
-        menuSlideBack(event.currentTarget,false)
-    })
-     function menuSlide(parent,from,isHorizontal){
+    bindEventMenuSlideBarHorizon()
+    bindEventMenuSlideBarVertical()
+    bindEventScrollTopButton()
+    bindEventToggleMenu()
+    bindEventToggleMenuItem()
+    activeSwiper()
+
+    function bindEventMenuSlideBarHorizon() {
+        $(".navMenu > .navMenu__item").hover((event) => {
+            let parent = event.currentTarget;
+            let center = parent.clientWidth / 2;
+            let enterPoint = event.offsetX;
+            let fromleft = enterPoint <= center;
+            menuSlide(parent, fromleft, true)
+        }, (event) => {
+            menuSlideBack(event.currentTarget, true)
+        })
+    }
+    function bindEventMenuSlideBarVertical() {
+        $(".navMenu__children > .navMenu__item").hover((event) => {
+            let parent = event.currentTarget;
+            let center = parent.clientHeight / 2;
+            let enterPoint = event.offsetY;
+            let fromTop = enterPoint <= center;
+            menuSlide(parent, fromTop, false)
+        }, (event) => {
+            menuSlideBack(event.currentTarget, false)
+        })
+    }
+
+    function menuSlide(parent, from, isHorizontal) {
         let startPos = from ? "-50%" : "50%";
-        let { className, prop } = isHorizontal ? 
-                                    {className : ".jq__menu__slide-h", prop: "left"} 
-                                    : {className : ".jq__menu__slide-v", prop: "top"};
+        let { className, prop } = isHorizontal ?
+            { className: ".jq__menu__slide-h", prop: "left" }
+            : { className: ".jq__menu__slide-v", prop: "top" };
         $(parent).find(className)
-                 .css({ [prop] : startPos, visibility : "visible" })
-                 .animate({ [prop] : 0 }, 100)
-     }
-     function menuSlideBack(parent,isHorizontal){
-        let { className, prop } = isHorizontal ? 
-                                    {className : ".jq__menu__slide-h", prop: "left"} 
-                                    : {className : ".jq__menu__slide-v", prop: "top"};
-        $(parent).find( className )
-                 .css({ [prop] : 0, visibility : "hidden" })
-     }
+            .css({ [prop]: startPos, visibility: "visible" })
+            .animate({ [prop]: 0 }, 100)
+    }
+    function menuSlideBack(parent, isHorizontal) {
+        let { className, prop } = isHorizontal ?
+            { className: ".jq__menu__slide-h", prop: "left" }
+            : { className: ".jq__menu__slide-v", prop: "top" };
+        $(parent).find(className)
+            .css({ [prop]: 0, visibility: "hidden" })
+    }
 
-     $("#jq-scroll-top").click((event)=>{
-         $("html").animate({
-             scrollTop : 0
-         },500)
-     })
-
-     $("#jq_slide_down").click( event =>{
-        $("#jq_slideDown_menu").slideToggle(300)
-                               .toggleClass("navMenu--mobile--active")
-     })
-     $(".jq_slide_down").click( event =>{
-        $(event.currentTarget).find('ul').slideToggle(300)
-     })
-
-     var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
+    function bindEventScrollTopButton() {
+        $("#jq-scroll-top").click((event) => {
+            $("html").animate({
+                scrollTop: 0
+            }, 500)
+        })
+    }
+    function bindEventToggleMenu() {
+        $("#jq_slide_down").click(event => {
+            $("#jq_slideDown_menu").slideToggle(300)
+                .toggleClass("navMenu--mobile--active")
+        })
+    }
+    function bindEventToggleMenuItem() {
+        $(".jq_slide_down").click(event => {
+            $(event.currentTarget).find('ul').slideToggle(300)
+        })
+    }
+    function activeSwiper(){
+        return new Swiper('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
     
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-        autoplay: {
-            delay: 5000,
-        },
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-    })
-
-
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            autoplay: {
+                delay: 5000,
+            },
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        })
+    }
 });
